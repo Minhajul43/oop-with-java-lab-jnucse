d71237b9bedf707bb35679c05db54e3a83db2f85
Minhajul43 <minhajul255685islam@gmail.com>
2026-07-25 21:49:57 +0600

    Add another file into new branch

---
+++ LabExamJnuCSE/practice.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class practice {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int i, j, k;
+    System.out.print("Enter the number of rows: ");
+    int rows = sc.nextInt();
+    System.out.print("Enter the number of columns: ");
+    int cols = sc.nextInt();
+    int[][] matrix = new int[rows][cols];
+    System.out.println("Enter the elements of the matrix:");
+    for (i = 0; i < rows; i++) {
+      for (j = 0; j < cols; j++) {
+        matrix[i][j] = sc.nextInt();
+      }
+    }
+    System.out.println("The matrix is:");
+    for (i = 0; i < rows; i++) {
+      for (j = 0; j < cols; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
