6e2d6b7806496219b9448a49fe4cb6adc8a94504
Minhajul43 <minhajul255685islam@gmail.com>
2026-07-26 07:19:55 +0600

    Edit some file into main branch

--- Lab-5/Calculation.java
+++ Lab-5/Calculation.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Scanner;
 class Calculation {
    int z;
  
@@ -8,7 +10,32 @@ class Calculation {
  
    public void subtraction(int x, int y) {
       z = x - y;
-      System.out.println("The difference between the given numbers:"+z);
+      System.out.println("The difference between the given numbers:" + z);
+   }
+   
+
+   public void multiplication(int x, int y) {
+      z = x * y;
+      System.out.println("The product of the given numbers:" + z);
+   }
+
+   public void division(int x, int y) {
+      z = x / y;
+      System.out.println("The quotient of the given numbers:" + z);
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+   
+      System.out.print("Enter first number:");
+      int a = sc.nextInt();
+      System.out.print("Enter second number:");
+      int b = sc.nextInt();
+      Calculation cal = new Calculation();
+      cal.addition(a, b);
+      cal.subtraction(a, b);
+      cal.multiplication(a, b);
+      cal.division(a, b);
    }
 }
 
